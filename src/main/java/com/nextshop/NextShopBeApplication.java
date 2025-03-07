c16a52e15a5169bc35d2585a3ca98d2ff590fdf7
suhyun <qkrtngus116@naver.com>
2025-03-07 14:06:07 +0900

    chore: PR템플릿 추가

--- src/main/java/com/nextshop/NextShopBeApplication.java
+++ src/main/java/com/nextshop/NextShopBeApplication.java
@@ -1,4 +1,4 @@
-package com.nextshopbe;
+package com.nextshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/nextshop/NextShopBeApplicationTests.java
+++ src/test/java/com/nextshop/NextShopBeApplicationTests.java
@@ -1,4 +1,4 @@
-package com.nextshopbe;
+package com.nextshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

4ae9e8e86a89d924e9fdf2d9a6ee9475a6d15b00
suhyun <qkrtngus116@naver.com>
2025-03-07 14:00:06 +0900

    git init

---
+++ src/main/java/com/nextshop/NextShopBeApplication.java
@@ -0,0 +1,13 @@
+package com.nextshopbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextShopBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextShopBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nextshop/NextShopBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextshopbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextShopBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

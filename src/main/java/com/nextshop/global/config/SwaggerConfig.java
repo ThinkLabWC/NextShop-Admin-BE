59a562ef8125c5537213a2d02e23e85cd9c72188
suhyun <qkrtngus116@naver.com>
2025-03-08 15:03:13 +0900

    feat: swagger설정

---
+++ src/main/java/com/nextshop/global/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.nextshop.global.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import io.swagger.v3.core.jackson.ModelResolver;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("NextShop Admin API")
+                .description("NextShop Admin API 명세서입니다.");
+
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
+    }
+
+    @Bean
+    public ModelResolver modelResolver(ObjectMapper objectMapper){
+        return new ModelResolver(objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE));
+    }
+}

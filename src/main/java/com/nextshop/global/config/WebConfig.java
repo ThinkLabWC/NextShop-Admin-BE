9e95b56424d348cf05ac5795029286cd85044ae9
suhyun <qkrtngus116@naver.com>
2025-03-08 22:56:15 +0900

    chore: naver-java-coding-convection 적용

--- src/main/java/com/nextshop/global/config/WebConfig.java
+++ src/main/java/com/nextshop/global/config/WebConfig.java
@@ -1,33 +1,35 @@
 package com.nextshop.global.config;
 
-import com.nextshop.global.interceptor.AuthorizationInterceptor;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
+import com.nextshop.global.interceptor.AuthorizationInterceptor;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-    private final AuthorizationInterceptor authorizationInterceptor;
+	private final AuthorizationInterceptor authorizationInterceptor;
 
-    private final List<String> DEFAULT_EXCLUDE = List.of(
-            "/",
-            "/error"
-    );
+	private final List<String> DEFAULT_EXCLUDE = List.of(
+		"/",
+		"/error"
+	);
 
-    private final List<String> SWAGGER = List.of(
-            "/swagger-ui.html",
-            "/swagger-ui/**"
-    );
+	private final List<String> SWAGGER = List.of(
+		"/swagger-ui.html",
+		"/swagger-ui/**"
+	);
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(authorizationInterceptor)
-                .excludePathPatterns(DEFAULT_EXCLUDE)
-                .excludePathPatterns(SWAGGER);
-    }
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(authorizationInterceptor)
+			.excludePathPatterns(DEFAULT_EXCLUDE)
+			.excludePathPatterns(SWAGGER);
+	}
 }
